d615d4d3277b2ef83bcda0e918636872c94c9bea
jewel <30555957+ishraque-hasan-jewel@users.noreply.github.com>
2022-09-29 21:17:56 +0600

    Finaly All Successfully

---
+++ src/components/App.js
@@ -0,0 +1,58 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "../styles/App.css";
+import Layout from "./Layout";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Quiz from "./pages/Quiz";
+import Result from "./pages/Result";
+import Signup from "./pages/Signup";
+import {AuthProvider} from "../contexts/AuthContext"
+
+function App() {
+  return (
+    <Router>
+      <AuthProvider>
+      <Layout>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="signup" element={<Signup />} />
+          <Route exact path="login" element={<Login />} />
+          <Route exact path="quiz" element={<Quiz />} />
+          <Route exact path="result" element={<Result />} />
+        </Routes>
+      </Layout>
+      </AuthProvider>
+    </Router>
+  );
+}
+export default App;
+//----------------------------------------
+// import ReactDOM from "react-dom/client";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import "../styles/App.css";
+// import Layout from "./Layout";
+// import Home from "./pages/Home";
+// import Login from "./pages/Login";
+// import Quiz from "./pages/Quiz";
+// import Result from "./pages/Result";
+// import Signup from "./pages/Signup";
+
+// export default function App() {
+//   return (
+//     <BrowserRouter>
+//       <Layout>
+//         <Routes>
+//           <Route path="/" element={<Home />}>
+//             <Route path="signup" element={<Signup />} />
+//             <Route path="login" element={<Login />} />
+//             <Route path="quiz" element={<Quiz />} />
+//             <Route path="result" element={<Result />} />
+//           </Route>
+//         </Routes>
+//       </Layout>
+//     </BrowserRouter>
+//   );
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<App />);
